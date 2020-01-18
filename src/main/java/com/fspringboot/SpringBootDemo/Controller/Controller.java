bdf4c99e1c9ef5d9b680e09fd9a4b1c753ba2670
15Venne <314652328@qq.com>
2020-01-18 10:49:51 +0800

    add dbs

--- src/main/java/com/fspringboot/SpringBootDemo/Controller/Controller.java
+++ src/main/java/com/fspringboot/SpringBootDemo/Controller/Controller.java
@@ -3,28 +3,71 @@ package com.fspringboot.SpringBootDemo.Controller;
 
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.CriteriaDefinition;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
+import com.fspringboot.SpringBootDemo.entity.User;
+
 @RestController
+@EnableAutoConfiguration
 @RequestMapping("/hello")
 public class Controller{
-	@RequestMapping("")
+	
+	@Autowired
+	MongoTemplate mongotemplate;
+	
+	
+	@RequestMapping("/getUser")
 	public JSONObject hello() {
 		
-		JSON json;
+		JSON json = new JSONObject();
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("resultCode", 1);
-		map.put("data", "hello" +'\n' +"avc"+ '\n' + "world");
+		
+		
+		Query query = new Query();
+		query.addCriteria(Criteria.where("name").is("15venne"));
+		if(mongotemplate.findOne(query, User.class) != null) {
+			User user = mongotemplate.findOne(query, User.class);
+			map.put("data",user);
+			
+			String name = mongotemplate.findOne(query, User.class).getName();
+			System.out.println("name: " + name);
+		}else {
+			map.put("data","");
+		}
+		
 		JSONObject object = (JSONObject) JSONObject.toJSON(map);
 		return object;
 	}
 	
-	@RequestMapping("/world")
-	public String helloWorld() {
-		return "11223344\n444";
+	@RequestMapping("/insert")
+	public JSONObject helloWorld(@RequestParam(defaultValue = "0")String name) {
+		User user = new User();
+		
+		user.setName(name);
+		user.setAge(22);
+		user.setPassword("456789");
+		user.setPhone("13999999999");
+		user.setSex(1);
+		user.setUsername("v314652328");
+		mongotemplate.insert(user);
+		
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("resultCode", 1);
+		JSONObject object = (JSONObject) JSONObject.toJSON(map);
+		return object;
+		
 	}
 }
---
+++ src/main/java/com/fspringboot/SpringBootDemo/entity/User.java
@@ -0,0 +1,91 @@
+package com.fspringboot.SpringBootDemo.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "user")
+public class User{
+	
+	@Id
+	private String id;
+	
+	@Field("username")
+	private String username="";
+	
+	private String password="vv123456";
+	
+	private String registerTime="1970-01-01";
+	
+	private String phone="";
+	
+	private String name="";
+	
+	private Integer sex=0;
+	
+	private Integer age=0;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getUsername(){
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getRegisterTime() {
+		return registerTime;
+	}
+	
+	public void setRegisterTime(String registerTime) {
+		this.registerTime = registerTime;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getSex() {
+		return sex;
+	}
+	
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
